191969a2ea949bc86f63db632fc0aea4a0c2074f
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2022-12-17 19:47:48 -0300

    Tanuki Soup in Lua

--- SOUP.lua
+++ SOUP.lua
@@ -1,15 +1,15 @@
 
 
 function FalseSoup(z,y,x)
-local tanukijiru = y + x
-local nisetanukijiru = z + tanukijiru
-if (nisetanukijiru == z + tanukijiru) then return true end
+local tanukijiru = y .. x
+local nisetanukijiru = z .. tanukijiru
+if (nisetanukijiru == z .. tanukijiru) then return true end
 end
 
 function FalseTanuki(z,y,x)
-local danukijiru = y + x
-local nisedanukijiru = z + danukijiru
-if (nisedanukijiru == z + danukijiru) then return true end
+local danukijiru = y .. x
+local nisedanukijiru = z .. danukijiru
+if (nisedanukijiru == z .. danukijiru) then return true end
 end
 
 FalseSoup("Nise","tanuki","jiru")
