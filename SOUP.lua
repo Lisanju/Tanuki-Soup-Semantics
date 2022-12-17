5fd50d63996abfaebe077785f338fc18175152dd
LIS-λ <106002045+Lisanju@users.noreply.github.com>
2022-12-17 19:52:21 -0300

    Update SOUP.lua

--- SOUP.lua
+++ SOUP.lua
@@ -7,9 +7,10 @@ if (nisetanukijiru == z .. tanukijiru) then return true end
 end
 
 function FalseTanuki(z,y,x)
-local danukijiru = y .. x
-local nisedanukijiru = z .. danukijiru
-if (nisedanukijiru == z .. danukijiru) then return true end
+local jiru = x
+local nisedanuki = z .. y
+local nisedanukijiru = nisedanuki .. jiru
+if (nisedanukijiru == nisedanuki .. jiru) then return true end
 end
 
 FalseSoup("Nise","tanuki","jiru")
